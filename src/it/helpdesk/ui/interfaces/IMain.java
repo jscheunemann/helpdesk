ba3340458fc65a7fa09bc5d3c9a10355923d431d
Jason Scheunemann <flyguy97@gmail.com>
2014-11-19 16:40:09 -0900

    Added files that I forgot to push

---
+++ src/it/helpdesk/ui/interfaces/IMain.java
@@ -0,0 +1,9 @@
+package it.helpdesk.ui.interfaces;
+
+public interface IMain {
+	public void openLoginDialog();
+	public void openLogoutDialog();
+	public void openCreateNewTicketDialog();
+	public void openEditTicketDialog();
+	public void close();
+}
---
+++ src/it/helpdesk/ui/interfaces/IMainMenu.java
@@ -0,0 +1,12 @@
+package it.helpdesk.ui.interfaces;
+
+public interface IMainMenu {
+	public void disableTicketMenu();
+	public void enableTicketMenu();
+	public void disableFileMenu();
+	public void enableFileMenu();
+	public void disableLoginMenuItem();
+	public void enableLoginMenuItem();
+	public void disableLogoutMenuItem();
+	public void enableLogoutMenuItem();
+}
