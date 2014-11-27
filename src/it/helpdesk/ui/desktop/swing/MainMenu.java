4eff9ceb6cb7f4e7e7d1714ee5582de0bc5f020b
cwhetzel <cwhetzel@student.umuc.edu>
2014-11-26 20:21:57 -0500

    Update MainMenu.java
    Added Javadoc documentation to the class.

--- src/it/helpdesk/ui/desktop/swing/MainMenu.java
+++ src/it/helpdesk/ui/desktop/swing/MainMenu.java
@@ -32,15 +32,55 @@ import javax.swing.JSeparator;
 import it.helpdesk.ui.interfaces.IMain;
 import it.helpdesk.ui.interfaces.IMainMenu;
 
+/**
+ * View class that controls the main menu toolbar for the application.
+ *
+ * @author	Helpdesk Tracker Team
+ * @version	1.0
+ * @since	2014-11-26
+ */
 public class MainMenu extends JMenuBar implements IMainMenu {
+	
+	/**
+	 * Contains the serial version number for the application.
+	 */
 	private static final long serialVersionUID = 6380669857111206438L;
+	
+	/**
+	 * Contains an IMain object.
+	 */
 	private IMain parentWindow;
+	
+	/**
+	 * Contains a JMenu object containing the file menu.
+	 */
 	private JMenu fileMenu;
+	
+	/**
+	 * Contains a JMenu object containing the ticket menu.
+	 */
 	private JMenu ticketMenu;
+	
+	/**
+	 * Contains a JMenuItem object to allow the user to logout.
+	 */
 	private JMenuItem logoutMenuItem;
+	
+	/**
+	 * Contains a JMenuItem object to allow the user to login.
+	 */
 	private JMenuItem loginMenuItem;
+	
+	/**
+	 * Contains a JMenuItem object to allow the user to exit the application.
+	 */
 	private JMenuItem exitMenuItem;
 
+	/**
+	 * Class constructor to build the main menu toolbar and the available options.
+	 * 
+	 * @param parentWindow a reference to the IMain parent object
+	 */
 	public MainMenu(IMain parentWindow) {
 		this.parentWindow = parentWindow;
 		fileMenu = new JMenu("File");
@@ -113,17 +153,26 @@ public class MainMenu extends JMenuBar implements IMainMenu {
 		this.add(ticketMenu);
 	}
 
+	/**
+	 * Method that will disable the ticket menu option.
+	 */
 	@Override
 	public void disableTicketMenu() {
 		this.ticketMenu.setEnabled(false);
 	}
 
+	/**
+	 * Method that will enable the ticket menu option.
+	 */
 	@Override
 	public void enableTicketMenu() {
 		this.ticketMenu.setEnabled(true);
 	}
 	
-		@Override
+	/**
+	 * Method that will disable the file menu option.
+	 */
+	@Override
 	public void disableFileMenu() {
 		for (Component c : this.fileMenu.getMenuComponents()) {
 			c.setEnabled(false);
@@ -131,6 +180,9 @@ public class MainMenu extends JMenuBar implements IMainMenu {
 		this.exitMenuItem.setEnabled(true);
 	}
 
+	/**
+	 * Method that will enable the file menu option.
+	 */
 	@Override
 	public void enableFileMenu() {
 		for (Component c : this.fileMenu.getMenuComponents()) {
@@ -138,21 +190,33 @@ public class MainMenu extends JMenuBar implements IMainMenu {
 		}
 	}
 
+	/**
+	 * Method that will disable the login menu item.
+	 */
 	@Override
 	public void disableLoginMenuItem() {
 		this.loginMenuItem.setEnabled(false);
 	}
 
+	/**
+	 * Method that will enable the login menu item.
+	 */
 	@Override
 	public void enableLoginMenuItem() {
 		this.loginMenuItem.setEnabled(true);
 	}
 
+	/**
+	 * Method that will disable the logout menu item.
+	 */
 	@Override
 	public void disableLogoutMenuItem() {
 		this.logoutMenuItem.setEnabled(false);
 	}
 
+	/**
+	 * Method that will enable the logout menu item.
+	 */
 	@Override
 	public void enableLogoutMenuItem() {
 		this.logoutMenuItem.setEnabled(true);
