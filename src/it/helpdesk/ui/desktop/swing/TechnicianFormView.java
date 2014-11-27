629ec03f7bc4fb986436adfa7cc0f37dcd29de24
cwhetzel <cwhetzel@student.umuc.edu>
2014-11-26 21:11:16 -0500

    Update TechnicianFormView.java
    Added Javadoc documentation to the class.

--- src/it/helpdesk/ui/desktop/swing/TechnicianFormView.java
+++ src/it/helpdesk/ui/desktop/swing/TechnicianFormView.java
@@ -30,18 +30,69 @@ import javax.swing.*;
 
 import it.helpdesk.ui.interfaces.*;
 
+/**
+ * View class that allows a user to create a new login for the application.
+ *
+ * @author	Helpdesk Tracker Team
+ * @version	1.0
+ * @since	2014-11-26
+ */
 public class TechnicianFormView implements ITechnicianFormView {
+	/**
+	 * Contains a ITechnicianFormController object.
+	 */
 	private ITechnicianFormController controller;
+	
+	/**
+	 * Contains a JDialog object for the current window
+	 */
 	private JDialog window;
+	
+	/**
+	 * Contains a JTextField object containing the username enterd by the user.
+	 */
 	private JTextField usernameTextBox;
+	
+	/**
+	 * Contains a JPasswordField object containing the password enterd by the user.
+	 */
 	private JPasswordField passwordTextBox;
+	
+	/**
+	 * Contains a JPasswordField object containing the password verification enterd by the user.
+	 */
 	private JPasswordField passwordConfirmationTextBox;
+	
+	/**
+	 * Contains a JTextField object containing the first name enterd by the user.
+	 */
 	private JTextField firstNameTextBox;
+	
+	/**
+	 * Contains a JTextField object containing the last name enterd by the user.
+	 */
 	private JTextField lastNameTextBox;
+	
+	/**
+	 * Contains a JTextField object containing the phone number enterd by the user.
+	 */
 	private JTextField phoneNumberTextBox;
+	
+	/**
+	 * Contains a JTextField object containing the email enterd by the user.
+	 */
 	private JTextField emailTextBox;
+	
+	/**
+	 * Contains a JButton object allowing the user to submit their request.
+	 */
 	private JButton okButton;
 	
+	/**
+	 * Class constructor creating the form for the user to setup a new login account.
+	 * 
+	 * @param parent contains a JFrame object referencing the parent window
+	 */
 	public TechnicianFormView(JFrame parent) {
 		window = new JDialog(parent, null, Dialog.ModalityType.APPLICATION_MODAL);
 		
@@ -146,11 +197,20 @@ public class TechnicianFormView implements ITechnicianFormView {
 		window.pack();
 	}
 
+	/**
+	 * Method to set the local controller object to the ITechnicianFormController passed
+	 * to the method.
+	 * 
+	 * @param controller contains a ITechnicianFormController object
+	 */
 	@Override
 	public void setController(ITechnicianFormController controller) {
 		this.controller = controller;
 	}
 
+	/**
+	 * Method to open the form and instantiate the Action Listener for the button-click event.
+	 */
 	@Override
 	public void open() {
 		okButton.addActionListener(new ActionListener() {
@@ -163,71 +223,137 @@ public class TechnicianFormView implements ITechnicianFormView {
 		this.window.setVisible(true);
 	}
 	
+	/**
+	 * Method to close the window.
+	 */
 	@Override
 	public void close() {
 		this.window.dispose();
 	}
 
+	/**
+	 * Method to retrieve the username entered by the user on the form.
+	 * 
+	 * @return a String containing the username entered by the user
+	 */
 	@Override
 	public String getUsername() {
 		return this.usernameTextBox.getText();
 	}
 
+	/**
+	 * Method to set the local username variable entered by the user on the form.
+	 * 
+	 * @param username contains the username value entered on the form
+	 */
 	@Override
 	public void setUsername(String username) {
 		this.usernameTextBox.setText(username);
 	}
 
+	/**
+	 * Method to retrieve the password entered by the user on the form.
+	 * 
+	 * @return a String containing the password entered by the user
+	 */
 	@Override
 	public String getPassword() {
 		return new String(this.passwordTextBox.getPassword());
 	}
 	
+	/**
+	 * Method to retrieve the password confirmation entered by the user on the form.
+	 * 
+	 * @return a String containing the password confirmation entered by the user
+	 */
 	@Override
 	public String getPasswordConfirmation() {
 		return this.passwordConfirmationTextBox.getPassword().toString();
 	}
 
+	/**
+	 * Method to retrieve the first name entered by the user on the form.
+	 * 
+	 * @return a String containing the first name entered by the user
+	 */
 	@Override
 	public String getFirstName() {
 		return this.firstNameTextBox.getText();
 	}
 
+	/**
+	 * Method to set the local first name variable entered by the user on the form.
+	 * 
+	 * @param firstName contains the first name value entered on the form
+	 */
 	@Override
 	public void setFirstName(String firstName) {
 		this.firstNameTextBox.setText(firstName);
 	}
 
+	/**
+	 * Method to retrieve the last name entered by the user on the form.
+	 * 
+	 * @return a String containing the last name entered by the user
+	 */
 	@Override
 	public String getLastName() {
 		return this.lastNameTextBox.getText();
 	}
 
+	/**
+	 * Method to set the local last name variable entered by the user on the form.
+	 * 
+	 * @param lastName contains the last name value entered on the form
+	 */
 	@Override
 	public void setLastName(String lastName) {
 		this.lastNameTextBox.setText(lastName);
 	}
 	
+	/**
+	 * Method to retrieve the phone number entered by the user on the form.
+	 * 
+	 * @return a String containing the phone number entered by the user
+	 */
 	@Override
 	public String getPhoneNumber() {
 		return this.phoneNumberTextBox.getText();
 	}
 	
+	/**
+	 * Method to set the local phone number variable entered by the user on the form.
+	 * 
+	 * @param phoneNumber contains the phone number value entered on the form
+	 */
 	@Override
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumberTextBox.setText(phoneNumber);
 	}
 
+	/**
+	 * Method to retrieve the email entered by the user on the form.
+	 * 
+	 * @return a String containing the email entered by the user
+	 */
 	@Override
 	public String getEmailAddress() {
 		return this.emailTextBox.getText();
 	}
 
+	/**
+	 * Method to set the local email address variable entered by the user on the form.
+	 * 
+	 * @param emailAddress contains the email address value entered on the form
+	 */
 	@Override
 	public void setEmailAddress(String emailAddress) {
 		this.emailTextBox.setText(emailAddress);
 	}
 
+	/**
+	 * Method to display validation error messages encountered during setup.
+	 */
 	@Override
 	public void showValidationErrorDialog() {
 		
