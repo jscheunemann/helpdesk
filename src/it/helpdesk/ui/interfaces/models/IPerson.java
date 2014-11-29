84dff40cc6950c4c479e836157b63955eede06aa
cwhetzel <cwhetzel@student.umuc.edu>
2014-11-29 16:28:30 -0500

    Update IPerson.java
    Added Javadoc documentation to the class.

--- src/it/helpdesk/ui/interfaces/models/IPerson.java
+++ src/it/helpdesk/ui/interfaces/models/IPerson.java
@@ -20,15 +20,81 @@
 
 package it.helpdesk.ui.interfaces.models;
 
+/**
+ * Model class to handle the communication between the application and the database.
+ * 
+ * @author	Helpdesk Tracker Team
+ * @version	1.0
+ * @since	2014-11-29
+ */
 public interface IPerson {
+	/**
+	 * Method to retrieve the current ID value of the user.
+	 * 
+	 * @return a long value with the current user ID
+	 */
 	public long getId();
+	
+	/**
+	 * Method to set the current user ID value.
+	 * 
+	 * @param id contains the user user ID
+	 */
 	public void setId(long id);
+	
+	/**
+	 * Method to retrieve the current first name value of the user.
+	 * 
+	 * @return a String value with the current user first name
+	 */
 	public String getFirstName();
+	
+	/**
+	 * Method to set the current user first name value.
+	 * 
+	 * @param firstName contains the user user first name
+	 */
 	public void setFirstName(String firstName);
+	
+	/**
+	 * Method to retrieve the current last name value of the user.
+	 * 
+	 * @return a String value with the current user last name
+	 */
 	public String getLastName();
+	
+	/**
+	 * Method to set the current user last name value.
+	 * 
+	 * @param lastName contains the user user last name
+	 */
 	public void setLastName(String lastName);
+	
+	/**
+	 * Method to retrieve the current phone number value of the user.
+	 * 
+	 * @return a String value with the current user phone number
+	 */
 	public String getPhoneNumber();
+	
+	/**
+	 * Method to set the current user phone number value.
+	 * 
+	 * @param phoneNumber contains the user user phone number
+	 */
 	public void setPhoneNumber(String phoneNumber);
+	
+	/**
+	 * Method to retrieve the current email value of the user.
+	 * 
+	 * @return a String value with the current user email
+	 */
 	public String getEmailAddress();
+	
+	/**
+	 * Method to set the current user email value.
+	 * 
+	 * @param emailAddress contains the user user email
+	 */
 	public void setEmailAddress(String emailAddress);
 }
