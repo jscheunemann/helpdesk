2a09f577e47cada00d42fc2388a1c8894f69b9a2
cwhetzel <cwhetzel@student.umuc.edu>
2014-11-29 16:42:47 -0500

    Update ITechnician.java
    Added Javadoc documentation to the class.

--- src/it/helpdesk/ui/interfaces/models/ITechnician.java
+++ src/it/helpdesk/ui/interfaces/models/ITechnician.java
@@ -20,9 +20,39 @@
 
 package it.helpdesk.ui.interfaces.models;
 
+/**
+ * Model class to handle the communication between the application and the database.
+ * 
+ * @author	Helpdesk Tracker Team
+ * @version	1.0
+ * @since	2014-11-29
+ */
 public interface ITechnician extends IPerson {
+	/**
+	 * Method to retrieve the current username value.
+	 * 
+	 * @return a String value containing the current username
+	 */
 	public String getUsername();
+	
+	/**
+	 * Method to set the current user's username to the value passed to the method.
+	 * 
+	 * @param username contains the current user's username
+	 */
 	public void setUsername(String username);
+	
+	/**
+	 * Method to retrieve the current user's password value.
+	 * 
+	 * @return a String value containing the current user's password
+	 */
 	public String getPassword();
+	
+	/**
+	 * Method to set the current user's password to the value passed to the method.
+	 * 
+	 * @param password contains the current user's password
+	 */
 	public void setPassword(String password);
 }
