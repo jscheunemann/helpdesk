f30001de069bf33b1b8f954a6c17867f8c5009bf
cwhetzel <cwhetzel@student.umuc.edu>
2014-11-29 18:11:24 -0500

    Update Ticket.java
    Added Javadoc documentation to the class.

--- src/it/helpdesk/main/Ticket.java
+++ src/it/helpdesk/main/Ticket.java
@@ -22,10 +22,28 @@ package it.helpdesk.main;
 
 import java.util.Date;
 
+/**
+ * View class that allows the user to add a new ticket to the system or update
+ * an existing ticket.
+ *
+ * @author	Helpdesk Tracker Team
+ * @version	1.0
+ * @since	2014-11-29
+ */
 public class Ticket {
+	
+	/**
+	 * Contains an enumerated list of the current priorities.
+	 */
 	public enum PriorityEnum{
 		LOW, MEDIUM, HIGH, URGENT;
 
+		/**
+		 * Method to retrieve the priority based on the index passed to the method.
+		 * 
+		 * @param i contains the index of the current priority
+		 * @return  an PriorityEnum value of the index
+		 */
 		public static PriorityEnum fromInt(int i) {
 			switch(i) {
 				case 0:
@@ -42,6 +60,12 @@ public class Ticket {
 		}
 	}
 
+	/**
+	 * Method to return the index value of the priority passed to the method.
+	 * 
+	 * @param j contains a PriorityEnum reference
+	 * @return  an int value of the PriorityEnum reference
+	 */
 	public static int toInt(PriorityEnum j){
 		switch(j) {
 			case LOW: 
@@ -57,9 +81,18 @@ public class Ticket {
 		return 0;
 	}
 
+	/**
+	 * Contains an enumerated list of the current service categories.
+	 */
 	public enum ServiceCatEnum {
 		ACCESS_ISSUE, HARDWARE, SOFTWARE, DATABASE, SOFTWARE_DEFECT, INQUIRY;
 
+		/**
+		 * Method to retrieve the service category based on the index passed to the method.
+		 * 
+		 * @param i contains the index of the current service cetegory
+		 * @return  an ServiceCatEnum value of the index
+		 */
 		public static ServiceCatEnum fromInt(int i) {
 			switch (i){
 				case 0: 
@@ -79,13 +112,19 @@ public class Ticket {
 			return null;    	
 		}
 
+		/**
+		 * Method to return the index value of the service category passed to the method.
+		 * 
+		 * @param j contains a ServiceCatEnum reference
+		 * @return  an int value of the ServiceCatEnum reference
+		 */
 		public static int toInt(ServiceCatEnum j) {
 			switch(j) {
 				case ACCESS_ISSUE: 
 					return 0;    			
 				case HARDWARE:
 					return 1;    			
-				case SOFTWARE:
+				case SOFTAWARE:
 					return 2;    			
 				case DATABASE:
 					return 3;  
@@ -99,9 +138,18 @@ public class Ticket {
 		}
 	};	
 
+	/**
+	 * Contains an enumerated list of the current statuses.
+	 */
 	public enum StatusEnum {
 		NEW, IN_PROGRESS, WAIT_FOR_PROCESS, WITHDRAWN, COMPLETE, DELETE;
 		
+		/**
+		 * Method to retrieve the status based on the index passed to the method.
+		 * 
+		 * @param i contains the index of the current status
+		 * @return  an StatusEnum value of the index
+		 */
 		public static StatusEnum fromInt(int i){
 			switch (i){
 				case 0: 
@@ -120,6 +168,12 @@ public class Ticket {
 			return null;    	
 		}
 
+		/**
+		 * Method to return the index value of the status passed to the method.
+		 * 
+		 * @param j contains a StatusEnum reference
+		 * @return  an int value of the StatusEnum reference
+		 */
 		public static int toInt(StatusEnum j){
 			switch(j) {
 				case NEW: 
@@ -140,125 +194,295 @@ public class Ticket {
 		}
 	};
 
+	/**
+	 * Contains the ticket's ID.
+	 */
 	private long ID;
+	
+	/**
+	 * Contains the user that opened the ticket.
+	 */
 	private String openedBy;
+	
+	/**
+	 * Contains the date the user opened the ticket.
+	 */
 	private Date openedDate;
+	
+	/**
+	 * Contains the client name who requested the ticket.
+	 */
 	private String client;
+	
+	/**
+	 * Contains the client phone number for the person who requested the ticket.
+	 */
 	private String clientPhone;
+	
+	/**
+	 * Contains the client email for the person who requested the ticket.
+	 */
 	private String clientEmail;
+	
+	/**
+	 * Contains an array of priorities.
+	 */
 	private PriorityEnum priority;
+	
+	/**
+	 * Contains an array of service categories.
+	 */
 	private ServiceCatEnum serviceCat;
+	
+	/**
+	 * Contains an array of statuses.
+	 */
 	private StatusEnum status;
+	
+	/**
+	 * Contains the date the ticket was completed.
+	 */
 	private Date completeDate;
+	
+	/**
+	 * Contains the description of the ticket.
+	 */
 	private String description;
+	
+	/**
+	 * Contains the log of all of the updates to the ticket.
+	 */
 	private String log;
+	
+	/**
+	 * Contains the summary of the ticket information.
+	 */
 	private String summary;
 
-
-
+	/**
+	 * Default class constructor that will initialize the ticket ID to zero.
+	 */
 	public Ticket() {
 		ID = 0;		
 	}
 
+	/**
+	 * Method to retrieve the current ticket ID.
+	 * 
+	 * @return a long value containing the ticket ID
+	 */
 	public long getID() {
 		return ID;
 	}
 
+	/**
+	 * Method to set the current ticket's ID value.
+	 * @param id contains the current ID information
+	 */
 	public void setID(long id) {
 		ID = id;
 	}
+	
+	/**
+	 * Method to retrieve the current ticket's opened by value.
+	 * 
+	 * @return a String value containing the ticket's opened by value
+	 */
 	public String getOpenedBy(){
 		return openedBy;
 	}
 
+	/**
+	 * Method to set the current ticket's opened by value.
+	 * @param opened_by contains the current opened by information
+	 */
 	public void setOpenedBy(String opened_by){
 		openedBy = opened_by;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's opened on value.
+	 * 
+	 * @return a Date value containing the ticket's opened on value
+	 */
 	public Date getOpenedDate(){
 		return openedDate;
 	}
 
+	/**
+	 * Method to set the current ticket's opened on value.
+	 * @param opened_date contains the current opened on information
+	 */
 	public void setOpenedDate(Date opened_date){
 		openedDate = opened_date;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's client name value.
+	 * 
+	 * @return a String value containing the ticket's client name value
+	 */
 	public String getClient(){
 		return client;
 	}
 
+	/**
+	 * Method to set the current ticket's client name value.
+	 * @param Client contains the current client name information
+	 */
 	public void setClient(String Client){
 		client = Client;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's client phone number value.
+	 * 
+	 * @return a String value containing the ticket's client phone number value
+	 */
 	public String getClientPhone(){
 		return clientPhone;
 	}
 
+	/**
+	 * Method to set the current ticket's client phone value.
+	 * @param client_phone contains the current client phone information
+	 */
 	public void setClientPhone(String client_phone){
 		clientPhone = client_phone;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's client email value.
+	 * 
+	 * @return a String value containing the ticket's client email value
+	 */
 	public String getClientEmail(){
 		return clientEmail;
 	}
 
+	/**
+	 * Method to set the current ticket's client email value.
+	 * @param client_email contains the current client email information
+	 */
 	public void setClientEmail(String client_email){
 		clientEmail = client_email;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's completed by value.
+	 * 
+	 * @return a Date value containing the ticket's completed by value
+	 */
 	public Date getCompleteDate(){
 		return completeDate;
 	}
 
+	/**
+	 * Method to set the current ticket's completed on value.
+	 * @param complete_date contains the current completed on information
+	 */
 	public void setCompleteDate(Date complete_date){
 		completeDate = complete_date;
 	}	
 
+	/**
+	 * Method to retrieve the current ticket's priority value.
+	 * 
+	 * @return a PriorityEnum value containing the ticket's priority value
+	 */
 	public PriorityEnum getPriority(){
 		return priority;
 	}
 
+	/**
+	 * Method to set the current ticket's priority value.
+	 * @param Priority contains the current priority information
+	 */
 	public void setPriority(PriorityEnum Priority){
 		priority = Priority;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's service category value.
+	 * 
+	 * @return a ServiceCatEnum value containing the ticket's service category value
+	 */
 	public ServiceCatEnum getServiceCat(){
 		return serviceCat;
 	}
 
+	/**
+	 * Method to set the current ticket's service category value.
+	 * @param serviceCategory contains the current service category information
+	 */
 	public void setServiceCat(ServiceCatEnum serviceCategory){
 		serviceCat = serviceCategory;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's status value.
+	 * 
+	 * @return a StatusEnum value containing the ticket's status value
+	 */
 	public StatusEnum getStatus(){
 		return status;		
 	}
 
+	/**
+	 * Method to set the current ticket's status value.
+	 * @param Status contains the current status information
+	 */
 	public void setStatus(StatusEnum Status){
 		status = Status;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's description value.
+	 * 
+	 * @return a String value containing the ticket's description value
+	 */
 	public String getDescription(){
 		return description;
 	}
 
+	/**
+	 * Method to set the current ticket's description value.
+	 * @param Description contains the current description information
+	 */
 	public void setDescription(String Description){
 		description = Description;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's summary value.
+	 * 
+	 * @return a String value containing the ticket's summary value
+	 */
 	public String getSummary(){
 		return summary;
 	}
-
+	
+	/**
+	 * Method to set the current ticket's summary value.
+	 * @param Summary contains the current summary information
+	 */
 	public void setSummary(String Summary){
 		summary = Summary;
 	}
 
+	/**
+	 * Method to retrieve the current ticket's log value.
+	 * 
+	 * @return a String value containing the ticket's log value
+	 */
 	public String getLog(){
 		return log;
 	}
 
+	/**
+	 * Method to set the current ticket's log value.
+	 * @param Log contains the current log information
+	 */
 	public void setLog(String Log){
 		log = Log;
 	}
