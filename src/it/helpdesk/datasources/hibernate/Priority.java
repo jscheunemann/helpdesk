d295573be9cab9d668a3db95d54e88a2097674c2
cwhetzel <cwhetzel@student.umuc.edu>
2014-11-29 18:54:21 -0500

    Update Priority.java
    Added Javadoc documentation to the class.

--- src/it/helpdesk/datasources/hibernate/Priority.java
+++ src/it/helpdesk/datasources/hibernate/Priority.java
@@ -27,27 +27,60 @@ import javax.persistence.Table;
 
 import it.helpdesk.ui.interfaces.models.IPriority;
 
+/**
+ * Model class to handle the communication between the application and the database.
+ * 
+ * @author	Helpdesk Tracker Team
+ * @version	1.0
+ * @since	2014-11-29
+ */
 @Entity
 @Table(name = "Priority")
 public class Priority implements IPriority {
+	/**
+	 * Contains the ID of the current record's priority
+	 */
 	@Id
 	@GeneratedValue
 	private long id;
 	
+	/**
+	 * Contains the name of the current record's priority
+	 */
 	private String priorityName;
 	
+	/**
+	 * Method to retrieve the ID of the current record's priority
+	 * 
+	 * @return a long value containing the ID of the current record's priority
+	 */
 	public long getId() {
 		return this.id;
 	}
 
+	/**
+	 * Method to set the ID of the current record's priority
+	 * 
+	 * @param id a long value containing the ID of the current record's priority
+	 */
 	public void setId(long id) {
 		this.id = id;
 	}
 
+	/**
+	 * Method to retrieve the name of the current record's priority
+	 * 
+	 * @return a String value containing the name of the current record's priority
+	 */
 	public String getPriorityName() {
 		return this.priorityName;
 	}
 
+	/**
+	 * Method to set the name of the current record's priority
+	 * 
+	 * @param priorityName a String value containing the name of the current record's priority
+	 */
 	public void setPriorityName(String priorityName) {
 		this.priorityName = priorityName;
 	}
