f788aea8180c3c17aeb187ad6df1e6176f30b4e7
cwhetzel <cwhetzel@student.umuc.edu>
2014-11-29 19:08:56 -0500

    Update ServiceCategory.java
    Added Javadoc documentation to the class.

--- src/it/helpdesk/datasources/hibernate/ServiceCategory.java
+++ src/it/helpdesk/datasources/hibernate/ServiceCategory.java
@@ -27,27 +27,61 @@ import javax.persistence.Table;
 
 import it.helpdesk.ui.interfaces.models.IServiceCategory;
 
+/**
+ * Model class to handle the communication between the application and the database.
+ * 
+ * @author	Helpdesk Tracker Team
+ * @version	1.0
+ * @since	2014-11-29
+ */
 @Entity
 @Table(name = "ServiceCategory")
 public class ServiceCategory implements IServiceCategory {
+	
+	/**
+	 * Contains the ID of the current record's service category
+	 */
 	@Id
 	@GeneratedValue
 	private long id;
 	
+	/**
+	 * Contains the name of the current record's service category
+	 */
 	private String serviceCategory;
 
+	/**
+	 * Method to retrieve the current record's service category ID.
+	 * 
+	 * @return a long value of the current record's service category
+	 */
 	public long getId() {
 		return this.id;
 	}
 
+	/**
+	 * Method to set the current record's service category ID.
+	 * 
+	 * @param id a long value of the current record's service category
+	 */
 	public void setId(long id) {
 		this.id = id;
 	}
 
+	/**
+	 * Method to retrieve the current record's service category name.
+	 * 
+	 * @return a String value of the current record's service category
+	 */
 	public String getCategoryName() {
 		return this.serviceCategory;
 	}
 
+	/**
+	 * Method to set the current record's service category name.
+	 * 
+	 * @param serviceCategory a String value of the current record's service category
+	 */
 	public void setCategoryName(String serviceCategory) {
 		this.serviceCategory = serviceCategory;
 	}
