dece505c24122e119bf6b4cbbfb89811dc0defa8
cwhetzel <cwhetzel@student.umuc.edu>
2014-11-29 20:15:27 -0500

    Update TechnicianDatasource.java
    Added Javadoc documentation to the class.

--- src/it/helpdesk/datasources/memory/TechnicianDatasource.java
+++ src/it/helpdesk/datasources/memory/TechnicianDatasource.java
@@ -25,18 +25,47 @@ import java.util.*;
 import it.helpdesk.ui.interfaces.models.*;
 import it.helpdesk.ui.interfaces.models.datasources.*;
 
+/**
+ * Model class to handle the communication between the application and the database.
+ * 
+ * @author	Helpdesk Tracker Team
+ * @version	1.0
+ * @since	2014-11-29
+ */
 public class TechnicianDatasource implements ITechnicianDatasource {
+	/**
+	 * Contains an array of ITechnician objects.
+	 */
 	private List<ITechnician> technicians = null;
 	
+	/**
+	 * Default class constructor that initializes the ITechnician object.
+	 */
 	public TechnicianDatasource() {
 		technicians = new Vector<ITechnician>();
 	}
 	
+	/**
+	 * Method to retrieve an array of technician objects.
+	 * 
+	 * @return an array of ITechnician objects
+	 */
 	@Override
 	public List<ITechnician> getTechnicians() {
 		return (List<ITechnician>) this.technicians;
 	}
 
+	/**
+	 * Method to save a new technician record.
+	 * 
+	 * @param technician contains an ITechnician object
+	 * @param username contains a String value with username
+	 * @param password contains a String value with password
+	 * @param firstName contains a String value with first name
+	 * @param lastName contains a String value with last name 
+	 * @param phoneNumber contains a String value with phone number
+	 * @param emailAddress contains a String value with email address
+	 */
 	@Override
 	public void saveTechnician(ITechnician technician, String username, String password, String firstName, String lastName, String phoneNumber, String emailAddress) {
 		long id = 1;
@@ -57,6 +86,12 @@ public class TechnicianDatasource implements ITechnicianDatasource {
 		}
 	}
 
+	/**
+	 * Method to retrieve an ITechnician object based on the username supplied
+	 * 
+	 * @param username contains the username to retrieve
+	 * @return         the ITechnician object for the username supplied
+	 */
 	@Override
 	public ITechnician getTechnicianByUsername(String username) {
 		ITechnician ret = null;
@@ -69,6 +104,13 @@ public class TechnicianDatasource implements ITechnicianDatasource {
 		return ret;
 	}
 
+	/**
+	 * Method to authenticate the username/password combination supplied by the user
+	 * 
+	 * @param username contains the username supplied by the user
+	 * @param password contains the password supplied by the user
+	 * @return         a boolean value showing whether the username/password combination matches the database
+	 */
 	@Override
 	public boolean checkPassword(String username, String password) {
 		System.out.println("The password is " + password);
@@ -88,6 +130,12 @@ public class TechnicianDatasource implements ITechnicianDatasource {
 		}
 	}
 
+	/**
+	 * Method used to determine whether or not the supplied username is available for use.
+	 * 
+	 * @param username contains the username supplied by the user
+	 * @return         a boolean value showing whether or not the username is available for use
+	 */
 	@Override
 	public boolean usernameAvailable(String username) {
 		boolean usernameExists = false;
