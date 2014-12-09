dbb5f03971bee2dd5589adb3113fc7a184f97bbc
jason <jason@192.168.1.79>
2014-12-09 01:25:00 -0900

    Added database preload routine to speed up credential's check.

---
+++ src/it/helpdesk/ui/desktop/swing/PreloadConnectionDialog.java
@@ -0,0 +1,53 @@
+package it.helpdesk.ui.desktop.swing;
+
+import java.awt.Dialog;
+
+import it.helpdesk.datasources.hibernate.HibernateDatasourceConfiguration;
+import it.helpdesk.ui.interfaces.IDatasourceConfiguration;
+
+import javax.swing.JDialog;
+import javax.swing.JProgressBar;
+import javax.swing.SwingWorker;
+import javax.swing.WindowConstants;
+
+public class PreloadConnectionDialog extends SwingWorker<Integer, Integer> {
+	private JDialog parent;
+	private JDialog dialog;
+	private JProgressBar progressBar = new JProgressBar();
+
+	public PreloadConnectionDialog(JDialog parent) {
+		this.parent = parent;
+		progressBar.setString("Waiting on database connection.");
+		progressBar.setStringPainted(true);
+		progressBar.setIndeterminate(true);
+		dialog = new JDialog(parent, "Connecting...", true);
+		dialog.getContentPane().add(progressBar);
+		dialog.pack();
+		
+	}
+
+	@Override
+	protected Integer doInBackground() throws Exception {
+		parent.setEnabled(false);
+		dialog.requestFocus();
+		dialog.setModal(false);
+		dialog.setResizable(false);
+		dialog.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		dialog.setLocationRelativeTo(parent);
+		dialog.setVisible(true);
+		IDatasourceConfiguration datasourceConfiguration = new HibernateDatasourceConfiguration();
+		datasourceConfiguration.getTechnicianDatasource().checkPassword("", "");
+		parent.setEnabled(true);
+		parent.requestFocus();
+		return 0;
+	}
+
+	@Override
+	protected void done() {
+		dialog.dispose();
+	}
+	
+	public JDialog getDialogObject() {
+		return this.dialog;
+	}
+}
